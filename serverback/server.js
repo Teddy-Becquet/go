29a019a409a77737a978c35ed2bfd1955b44f0cc
Jonas <site1@gmail.com>
2025-03-31 17:40:38 +0200

    comme

--- serverback/server.js
+++ serverback/server.js
@@ -27,6 +27,79 @@ const limiter = rateLimit({
     message: "hahahahah!!!! Trop de tentatives. Réessayez plus tard.",
 });
 
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cookieParser());
+app.use(express.json());
+app.use(limiter); // Appliquer à toutes les routes
+
+// Middleware d'authentification
+function authenticateToken(req, res, next) {
+    const token = req.cookies.token; // Récupérer le token depuis les cookies
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+//Connexion à la base de données
+const bddConnection = mysql.createConnection({
+    host: '192.168.64.175',
+    user: 'site1',
+    password: 'yuzu007',
+    database: 'Classements'
+});
+
+bddConnection.connect(function (err) {
+    if (err) throw err;
+    console.log("Vous êtes enfin connecté sur le serveur !");
+});
+
+// Route à emprunter dans le navigateur
+app.get('/', (req, res) => {
+    res.json('Bonjour, ceci est notre serveur (back-end), soyez les bienvenus ! ajouter un /accueil dans URL pour accéder à la page d\'accueil');
+});
+
+// Route pour l'inscription
+app.post('/inscription', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { username: req.body.username, password: hashedPassword };
+        bddConnection.query('SELECT * FROM users WHERE user_name = ?', [username], async function (err, rows) {
+            if (err) throw err;
+            res.status(201).send('Utilisateur enregistré');
+        });
+    } catch {
+        res.status(500).json();
+    }
+});
+
+// Route pour la connexion
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    bddConnection.query('SELECT * FROM users WHERE username = ?', [username], async function (err, rows) {
+        if (err) throw err;
+        if (rows.length === 0) return res.status(400).send('Utilisateur non trouvé');
+        try {
+            if (await bcrypt.compare(password, rows[0].password)) {
+                const user = { name: username };
+                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+                res.cookie('token', accessToken, { httpOnly: true }); // Stocker le token dans un cookie
+                res.json({ accessToken: accessToken });
+            } else {
+                res.json('Mot de passe incorrect'); // ici j'ai changé le status en "send" en json
+            }
+        } catch {
+            res.status(500).send();
+        }
+    });
+});
+
 // Route sécurisée pour l'ajout d'admin avec hachage du mot de passe
 app.post('/admin/connexion', async (req, res) => {
     const { nom, mdp } = req.body;
@@ -124,79 +197,6 @@ app.delete('/admin/users/:id', (req, res) => {
         }
         res.status(200).json({ message: "Utilisateur supprimé !" });
     });
-});
- 
-// Middleware
-app.use(cors());
-app.use(express.urlencoded({ extended: true })); 
-app.use(cookieParser());
-app.use(express.json());
-app.use(limiter); // Appliquer à toutes les routes
-
-//Connexion à la base de données
-const bddConnection = mysql.createConnection({
-    host: '192.168.64.175',
-    user: 'site1',
-    password: 'yuzu007',
-    database: 'Classement'
-});
-
-bddConnection.connect(function (err) {
-    if (err) throw err;
-    console.log("Vous êtes enfin connecté sur le serveur !");
-});
-
-// Middleware d'authentification
-function authenticateToken(req, res, next) {
-    const token = req.cookies.token; // Récupérer le token depuis les cookies
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
-
-// Route pour l'inscription
-app.post('/inscription', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = { username: req.body.username, password: hashedPassword };
-        bddConnection.query('SELECT * FROM users WHERE user_name = ?', [username], async function (err, rows) {
-            if (err) throw err;
-            res.status(201).send('Utilisateur enregistré');
-        });
-    } catch {
-        res.status(500).json();
-    }
-});
-
-// Route pour la connexion
-app.post('/login', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    bddConnection.query('SELECT * FROM users WHERE username = ?', [username], async function (err, rows) {
-        if (err) throw err;
-        if (rows.length === 0) return res.status(400).send('Utilisateur non trouvé');
-        try {
-            if (await bcrypt.compare(password, rows[0].password)) {
-                const user = { name: username };
-                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-                res.cookie('token', accessToken, { httpOnly: true }); // Stocker le token dans un cookie
-                res.json({ accessToken: accessToken });
-            } else {
-                res.json('Mot de passe incorrect'); // ici j'ai changé le status en "send" en json
-            }
-        } catch {
-            res.status(500).send();
-        }
-    });
-});
-
-// Route à emprunter dans le navigateur
-app.get('/192.168.64.175', (req, res) => { // ici j'ai changer le "/" en "/192.168.64.175"
-    res.json('Bonjour, ceci est notre serveur (back-end), soyez les bienvenus ! ajouter un /accueil dans URL pour accéder à la page d\'accueil');
 });
 
 // Route pour la page d'accueil = index.html (protégée)
